e9410339ceb88b0b650c4d10854fe46dd54e8622
Nabeel Ahmad <Nabeel.Y.Ahmad.-ND@disney.com>
2021-01-26 19:26:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +7,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
